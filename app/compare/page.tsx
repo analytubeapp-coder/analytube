c0b5ce0659adc9672823f309410a1a0792670e93
Mina <analytubeapp@gmail.com>
2025-11-24 20:46:53 +0330

    update & add compare

---
+++ app/compare/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+export default function ComparePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 p-4">
+      <h1 className="text-2xl md:text-4xl font-bold text-gray-800">
+        تست صفحه Compare
+      </h1>
+    </div>
+  );
+}
