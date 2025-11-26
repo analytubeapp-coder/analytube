18354e102e170fe4dfc0fcb7616271ff947b22fe
Mina <analytubeapp@gmail.com>
2025-11-26 17:44:56 +0330

    update

--- components/dashboard/SearchBar.tsx
+++ components/dashboard/SearchBar.tsx
@@ -15,7 +15,7 @@ export default function SearchBar() {
   };
 
   return (
-    <div className="flex items-center w-full max-w-[650px] bg-[#f5f5f5] rounded-full overflow-hidden">
+    <div className="flex items-center w-full max-w-[600px] bg-[#ffffff] rounded-full overflow-hidden">
 
       {/* INPUT (same as homepage, just reduced padding) */}
       <input
