583abcf8bb2c8acc145f738d2c5bcee9c322a283
Mina <analytubeapp@gmail.com>
2025-12-03 17:36:26 +0330

    update

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -65,7 +65,7 @@ export default function SignUpPage() {
         <div
           className="
             absolute top-[35%] left-[55%]
-            w-[2000px] h-[650px]
+            w-[550px] md:w-[2000px] h-[450px] md:h-[650px]
             -translate-x-1/2 -translate-y-1/2
             rotate-[25deg]
             rounded-[9999px] blur-[150px] opacity-60
@@ -80,7 +80,7 @@ export default function SignUpPage() {
         <div
           className="
             absolute top-[60%] left-[40%]
-            w-[1200px] h-[650px]
+            w-[550px] md:w-[1200px] h-[450px] md:h-[650px]
             -translate-x-1/2 -translate-y-1/2
             rotate-[-30deg]
             rounded-[9999px] blur-[150px] opacity-60
